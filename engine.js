100d840477c47b1f966e6222c69a7e3016b7faa1
Shiniga-OP <shinigadev@gmail.com>
2025-03-28 07:23:35 -0300

    Adição de partículas

--- engine.js
+++ engine.js
@@ -6,7 +6,8 @@ class Engine {
       this.canvas = document.querySelector("canvas");
     }
     
-    this.contexto = this.canvas.getContext("2d");
+    this.ctx = this.canvas.getContext("2d");
+    this.ctx
     this.texto = [];
     this.camadas = [];
     this.camada1 = this.novaCamada();
@@ -22,23 +23,37 @@ class Engine {
     return camada;
   }
   
-  novoSprite(png, camada) {
-    const sprite = [{
+  novaParticula(sprite, camada) {
+    const particula = new Particula(null, sprite);
+    if(!camada) {
+      camada.push(particula);
+    } else {
+      this.camada1.push(particula);
+    }
+  }
+  
+  novoSprite(png, camada, x=0, y=0, escalaX=32, escalaY=32, auto=true) {
+    const sprite = {
       imagem: new Image(),
       x: 0,
       y: 0,
       escalaX: 16,
       escalaY: 16
-    }];
+    };
     
-    sprite[0].imagem.src = png;
-
-    if(!camada) {
-      this.camada1.push(sprite[0]);
-    } else {
-      camada.push(sprite[0]);
+    sprite.imagem.src = png;
+    sprite.x=x;
+    sprite.y=y;
+    sprite.escalaX=escalaX;
+    sprite.escalaY=escalaY;
+    if(auto==true) {
+      if(!camada) {
+        camada.push(sprite);
+      } else {
+        this.camada1.push(sprite);
+      }
     }
-    return sprite[0];
+    return sprite;
   }
   
   comecarAnimacao(alvo, animacao, intervalo=0.5, inicio=0) {
@@ -52,31 +67,33 @@ class Engine {
   }
   
   renderizar(canvasOculto) {
-    this.contexto.clearRect(0, 0, this.canvas.width, this.canvas.height);
+    this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
     
     for(const camada of this.camadas) {
-      for(const elemento of camada) {
-        if(elemento.imagem && elemento.imagem.complete) {
-          this.contexto.drawImage(
-            elemento.imagem,
-            elemento.x, elemento.y,
-            elemento.escalaX, elemento.escalaY
+      for(const sprite of camada) {
+        if(sprite.imagem && sprite.imagem.complete) {
+          this.ctx.drawImage(
+            sprite.imagem,
+            sprite.x, sprite.y,
+            sprite.escalaX, sprite.escalaY
           );
-        }
-        if(elemento.texto) {
-          this.contexto.fillText(
-            elemento.texto,
-            elemento.x, elemento.y,
-            elemento.escalaX, elemento.escalaY
+        } else if(sprite.texto) {
+          this.ctx.fillText(
+            sprite.texto,
+            sprite.x, sprite.y,
+            sprite.escalaX, sprite.escalaY
           );
-          this.contexto.font = elemento.escala;
-          this.contexto.fillStyle = elemento.cor;
+          this.ctx.font = sprite.escala;
+          this.ctx.fillStyle = sprite.cor;
+        } else if(sprite instanceof Particula) {
+          sprite.desenhar(this.ctx);
+          sprite.atualizar(this.canvas.width, this.canvas.height);
         }
       }
     }
     
     if(canvasOculto) {
-      this.contexto.drawImage(canvasOculto, 0, 0);
+      this.ctx.drawImage(canvasOculto, 0, 0);
     }
     if(this.renderizacao) {
       requestAnimationFrame(() => this.renderizar(canvasOculto), this.canvas);
@@ -174,7 +191,7 @@ class Engine {
 }
   
   novoTexto(escrita, tamanho="30px", coloracao="blue", camada) {
-    const texto = [{
+    const texto = {
       texto: escrita,
       cor: coloracao,
       x: 100,
@@ -182,36 +199,41 @@ class Engine {
       escala: tamanho+" Ariel",
       escalaX: 228,
       escalaY: 32
-    }];
+    };
     if(!camada) {
-      this.camada1.push(texto[0]);
+      this.camada1.push(texto);
     } else {
-      camada.push(texto[0]);
+      camada.push(texto);
     }
   }
   
-  novoBotao(png, estado="click", funcao, camada) {
-    const sprite = [{
+  novoBotao(png, estado="click", funcao, camada, x=0, y=0, escalaX=16, escalaY=16) {
+    const sprite = {
       imagem: new Image(),
       x: 0,
       y: 0,
       escalaX: 16,
       escalaY: 16
-    }];
-    sprite[0].imagem.src = png;
+    };
+    sprite.imagem.src=png;
+    sprite.x=x;
+    sprite.y=y;
+    sprite.escalaX=escalaX;
+    sprite.escalaY=escalaY;
+    
     if(estado==="click") {
-      sprite[0].imagem.onload = () => {
+      sprite.imagem.onload = () => {
         this.canvas.addEventListener('touchstart', (evento) => {
           evento.preventDefault();
           
           const toqueX = evento.touches[0].clientX - this.canvas.getBoundingClientRect().left;
           const toqueY = evento.touches[0].clientY - this.canvas.getBoundingClientRect().top;
           
-          if (
-            toqueX >= sprite[0].x &&
-            toqueX <= sprite[0].x + sprite[0].escalaX &&
-            toqueY >= sprite[0].y &&
-            toqueY <= sprite[0].y + sprite[0].escalaY
+          if(
+            toqueX >= sprite.x &&
+            toqueX <= sprite.x + sprite.escalaX &&
+            toqueY >= sprite.y &&
+            toqueY <= sprite.y + sprite.escalaY
             ) {
               funcao(evento);
             }
@@ -225,11 +247,11 @@ class Engine {
         const toqueX = evento.touches[0].clientX - this.canvas.getBoundingClientRect().left;
         const toqueY = evento.touches[0].clientY - this.canvas.getBoundingClientRect().top;
         
-        if (
-          toqueX >= sprite[0].x &&
-          toqueX <= sprite[0].x + sprite[0].escalaX &&
-          toqueY >= sprite[0].y &&
-          toqueY <= sprite[0].y + sprite[0].escalaY
+        if(
+          toqueX >= sprite.x &&
+          toqueX <= sprite.x + sprite.escalaX &&
+          toqueY >= sprite.y &&
+          toqueY <= sprite.y + sprite.escalaY
           ) {
             pressionado = true;
             
@@ -237,7 +259,7 @@ class Engine {
             
             function loop(funcao) {
               setTimeout(() => {
-                if (pressionado) {
+                if(pressionado) {
                   funcao();
                   requestAnimationFrame(() => loop(funcao));
                 }
@@ -252,12 +274,12 @@ class Engine {
     }
     
     if(!camada) {
-      this.camada1.push(sprite[0]);
+      this.camada1.push(sprite);
     } else {
-      camada.push(sprite[0]);
+      camada.push(sprite);
     }
 
-    return sprite[0];
+    return sprite;
   }
   
   ajustarTela(escalaX="100%", escalaY="100%") {
@@ -272,6 +294,8 @@ class Engine {
     this.canvas.width = window.innerWidth;
     this.canvas.height = window.innerHeight;
   }
+  
+  
   
   repetirAte(condicao, funcao) {
     while(condicao) {
@@ -301,4 +325,118 @@ class Engine {
   mudarTela(tela) {
     window.location.href = tela;
   }
+}
+
+class Sprite {
+  constructor(caminho, x=0, y=0, escalaX=32, escalaY=32) {
+    this.imagem = new Image();
+    this.imagem.src = caminho;
+    this.x = x;
+    this.y = y;
+    this.escalaX = escalaX;
+    this.escalaY = escalaY;
+  }
+}
+
+class Particula {
+    constructor(cor, caminho, velocidadeX=100, velocidadeY=100, vida=200) {
+        this.cor = cor;
+        this.sprite = new Sprite(caminho);
+        this.velocidadeX = velocidadeX;
+        this.velocidadeY = velocidadeY;
+        this.vida = vida;
+        this.tamanho = Math.random() * 5 + 2;
+    }
+
+    atualizar(larguraCanvas, alturaCanvas) {
+      this.comportamento();
+        this.vida -= 1;
+
+        if(this.sprite.x < 0 || this.sprite.x > larguraCanvas || this.sprite.y < 0 || this.sprite.y > alturaCanvas || this.vida <= 0) {
+            return false;
+        }
+        return true;
+    }
+    
+    comportamento() {
+      this.sprite.x += Math.random()*5;
+      this.sprite.y += Math.random()*5;
+    }
+
+    desenhar(ctx) {
+      if(this.sprite !== null) {
+        ctx.drawImage(
+          this.sprite.imagem,
+          this.sprite.x,
+          this.sprite.y,
+          this.sprite.escalaX,
+          this.sprite.escalaY
+          )
+      }
+      if(this.cor !== null) {
+        ctx.fillStyle = this.cor;
+        ctx.beginPath();
+        ctx.arc(this.sprite.x, this.sprite.y, this.tamanho, 0, Math.PI * 2);
+        ctx.fill();
+      }
+    }
+}
+
+class ArrastavelHtml {
+    constructor(id, tamanhoGrade) {
+        this.id = id;
+        this.elemento = document.getElementById(id);
+        
+        this.x = document.createElement('h1');
+        this.x.id = 'posX';
+        document.body.appendChild(this.x);
+        this.y = document.createElement('h1');
+        this.y.id = 'posY';
+        document.body.appendChild(this.y);
+        
+        this.x.style.position = 'absolute';
+        this.y.style.position = 'absolute';
+        this.x.style.transform = 'translate(100px, 100px)';
+        this.y.style.transform = 'translate(200px, 100px)';
+        
+        this.tamanhoGrade = tamanhoGrade;
+        this.arrastando = false;
+        
+        this.elemento.style.position = 'absolute';
+        
+        this.elemento.addEventListener('touchstart', (evento) => this.iniciarArrasto(evento));
+        document.addEventListener('touchend', () => this.arrastando = false);
+        document.addEventListener('touchmove', (evento) => this.arrastar(evento));
+    }
+    
+    iniciarArrasto(evento) {
+        evento.preventDefault();
+        
+        const toque = evento.touches[0];
+        
+        this.posX = this.posX || this.elemento.offsetLeft;
+        this.posY = this.posY || this.elemento.offsetTop;
+        
+        this.deslocX = toque.clientX - this.posX
+        this.deslocY = toque.clientY - this.posY
+        
+        this.arrastando = true;
+    }
+
+    arrastar(evento) {
+        if(this.arrastando) {
+            const toque = evento.touches[0];
+            
+            this.posX = toque.clientX - this.deslocX;
+            this.posY = toque.clientY - this.deslocY;
+            
+            this.posX = Math.round(this.posX / this.tamanhoGrade) * this.tamanhoGrade;
+            this.posY = Math.round(this.posY / this.tamanhoGrade) * this.tamanhoGrade;
+            
+            this.elemento.style.transform = `translate(${this.posX}px, ${this.posY}px)`;
+            
+            this.x.textContent = "X: " + this.posX;
+            this.y.textContent = "Y: " + this.posY;
+        }
+    }
 }
